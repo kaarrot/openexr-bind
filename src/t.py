87913a7454be4eee2a7e916e3cb198590bf8a5df
kubaroth <kubaroth@users.noreply.github.com>
2019-08-23 13:09:16 -0700

    wip

--- src/t.py
+++ src/t.py
@@ -5,9 +5,34 @@ import example
 example.add(2,3)
 print example.vec_test_cpp()
 example.test_header()
+image = example.InputFile("/home/kuba/SRC/openexr-bind/build/openexr_src/IlmImfTest/lineOrder_decreasing.exr", 1)
+header =  image.header()
 
-aa = example.InputFile("/home/kuba/PRJ/openexr-pybind/build/openexr_src/IlmImfTest/lineOrder_decreasing.exr", 1)
-aa.hh()
-
-bb = example.TypedAttribute("BB")
+bb = example._StringAttribute("BB")
 bb.value()
+
+print header.iter()
+
+
+print header.getAttrib("displayWindow")
+
+# Box2i
+#aa = example.Box2i(Imath.V2i(0,1),Imath.V2i(2,3))
+
+
+## Create Box2iAttribute
+v1 = Imath.V2i(2,3)
+v2 = Imath.V2i(3,4)
+print Imath.Box2i(v1,v2)
+# example.Attrib(Box2i ???
+
+
+
+
+'''
+ b /home/kuba/SRC/openexr-bind/src/example.cpp:190
+
+    Header header (width, height);
+    header.insert ("comments", StringAttribute (comments));
+    header.insert ("cameraTransform", M44fAttribute (cameraTransform));
+'''
